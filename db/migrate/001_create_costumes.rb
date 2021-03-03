f86f1de0340a1a1ed8a7e9f9da5fcabcf17132b2
Vanessa Fiechtner <fiechtnervanessa@gmail.com>
2021-03-03 02:41:52 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -12,7 +12,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
-      t.size :size
+      t.string :size
       t.string :image_url
     end
   end
